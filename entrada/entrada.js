facc54756f8bed33806ceb000f2327a7ad8009ed
RYAN LEONARDO FERNANDES MARTINS <00001073887352sp@aluno.educacao.sp.gov.br>
2024-08-19 15:23:31 -0300

    CU

--- entrada/entrada.js
+++ entrada/entrada.js
@@ -1,3 +1,33 @@
 document.getElementById("pudim").onclick = function () {
-        location.href = "../pudim/pudim.html";
-  }
+    location.href = "../pudim/pudim.html";
+}
+document.getElementById("enviar").addEventListener("click", function() {
+    location.href = "../botao/botao.html";
+});
+document.getElementById("enviar1").addEventListener("click", function() {
+    location.href = "../botao/botao.html";
+});
+document.getElementById("enviar2").addEventListener("click", function() {
+    location.href = "../botao/botao.html";
+});
+document.getElementById("enviar3").addEventListener("click", function() {
+    location.href = "../botao/botao.html";
+});
+document.getElementById("enviar4").addEventListener("click", function() {
+    location.href = "../botao/botao.html";
+});
+document.getElementById("enviar5").addEventListener("click", function() {
+    location.href = "../botao/botao.html";
+});
+document.getElementById("enviar6").addEventListener("click", function() {
+    location.href = "../botao/botao.html";
+});
+document.getElementById("enviar7").addEventListener("click", function() {
+    location.href = "../botao/botao.html";
+});
+document.getElementById("enviar8").addEventListener("click", function() {
+    location.href = "../botao/botao.html";
+});
+document.getElementById("enviar9").addEventListener("click", function() {
+    location.href = "../botao/botao.html";
+});
