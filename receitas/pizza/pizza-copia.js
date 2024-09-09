b658efaf68383f72f06f3fa1d65228afc1286cfe
RYAN LEONARDO FERNANDES MARTINS <00001073887352sp@aluno.educacao.sp.gov.br>
2024-09-09 13:19:25 -0300

    quase qwq

---
+++ receitas/pizza/pizza-copia.js
@@ -0,0 +1,3 @@
+document.getElementById("voltar").addEventListener("click", function() {
+    location.href = "./pizza.html";
+})
--- receitas/pizza/pizza.js
+++ receitas/pizza/pizza.js
@@ -1,30 +1,30 @@
 document.getElementById("enviar").addEventListener("click", function() {
-    location.href = "../pudim/pudim-copia.html";
+    location.href = "../pizza/pizza-copia.html";
 });
 document.getElementById("enviar1").addEventListener("click", function() {
-    location.href = "../pudim/pudim-copia.html";
+    location.href = "../pizza/pizza-copia.html";
 });
 document.getElementById("enviar2").addEventListener("click", function() {
-    location.href = "../pudim/pudim-copia.html";
+    location.href = "../pizza/pizza-copia.html";
 });
 document.getElementById("enviar3").addEventListener("click", function() {
-    location.href = "../pudim/pudim-copia.html";
+    location.href = "../pizza/pizza-copia.html";
 });
 document.getElementById("enviar4").addEventListener("click", function() {
-    location.href = "../pudim/pudim-copia.html";
+    location.href = "../pizza/pizza-copia.html";
 });
 document.getElementById("enviar5").addEventListener("click", function() {
-    location.href = "../pudim/pudim-copia.html";
+    location.href = "../pizza/pizza-copia.html";
 });
 document.getElementById("enviar6").addEventListener("click", function() {
-    location.href = "../pudim/pudim-copia.html";
+    location.href = "../pizza/pizza-copia.html";
 });
 document.getElementById("enviar7").addEventListener("click", function() {
-    location.href = "../pudim/pudim-copia.html";
+    location.href = "../pizza/pizza-copia.html";
 });
 document.getElementById("enviar8").addEventListener("click", function() {
-    location.href = "../pudim/pudim-copia.html";
+    location.href = "../pizza/pizza-copia.html";
 });
 document.getElementById("enviar9").addEventListener("click", function() {
-    location.href = "../pudim/pudim-copia.html";
+    location.href = "../pizza/pizza-copia.html";
 });
